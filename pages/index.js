bf0d3653a3eacb8c873d476c165490943ffa3f95
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2021-03-29 15:13:30 +0200

    Getting the currentUser as SSR props

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
-export const LandingPage = () => (
+export const LandingPage = ({ currentUser }) => (
   <div>
-    NextJS landing
+    {currentUser.email}
   </div>
 );
 
-// LandingPage.getInitialProps = () => {
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',
+      {
+        headers: req.headers
+      }
+    );
 
-// };
+    return data;
+  }
+
+  const { data } = await axios.get('/api/users/currentUser');
+
+  return data;
+};
 
 export default LandingPage;
